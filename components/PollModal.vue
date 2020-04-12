7a04d9be5300f37f6733d01b13d3f2b2c9ff3e9c
Lautaro Carro <lautaroecarro@gmail.com>
2020-04-12 18:35:03 -0300

    modify: Poll Modal

--- components/PollModal.vue
+++ components/PollModal.vue
@@ -1,10 +1,32 @@
 <template>
   <modal name="hello-world">
-    <h1>¿Cuantos dedos tengo?</h1>
-    <h4>5</h4>
-    <h4>10</h4>
-    <h4>20</h4>
-    <h4>40</h4>
+    <div class="container">
+      <div class="row title">
+        <div class="col col-lg-12">
+          <p class="text-center">{{poll.Question}}</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col col-lg-12">
+          <a href="#" class="square_btn">{{poll.Options.Option1.Text}}</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col col-lg-12">
+          <a href="#" class="square_btn">{{poll.Options.Option2.Text}}</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col col-lg-12">
+          <a href="#" class="square_btn">{{poll.Options.Option3.Text}}</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col col-lg-12">
+          <a href="#" class="square_btn">{{poll.Options.Option4.Text}}</a>
+        </div>
+      </div>
+    </div>
   </modal>
 </template>
 
@@ -14,12 +36,12 @@ export default {
   name: "pollmodal",
   data() {
     return {
-      polls: null
+      poll: null
     };
   },
   watch: {
-    polls: function(newValue) {
-      if (newValue.id == 1) {
+    poll: function(newValue) {
+      if (this.poll.Show) {
         this.show();
       }
     }
@@ -30,10 +52,37 @@ export default {
     }
   },
   firebase: {
-    polls: db.ref("polls")
+    poll: db.ref("Poll")
   }
 };
 </script>
 
 <style scoped>
+.v--modal-overlay {
+  background: rgba(0, 0, 0, 0.7);
+}
+.container {
+  margin-top: 4vh;
+}
+.row {
+  font-size: larger;
+  height: auto;
+  margin: 0 6vw;
+}
+.square_btn {
+  display: inline-block;
+  padding: 7px 20px;
+  border-radius: 25px;
+  border: solid 8px;
+  text-decoration: none;
+  color: #fff;
+  background-image: -webkit-linear-gradient(45deg, #ea07ff 0%, #7f3fc7 100%);
+  background-image: linear-gradient(45deg, #ea07ff 0%, #7f3fc7 100%);
+  transition: 0.4s;
+  width: 100%;
+}
+.square_btn:hover {
+  background-image: -webkit-linear-gradient(45deg, #ea07ff 0%, #3f1f64 100%);
+  background-image: linear-gradient(45deg, #ea07ff 0%, #3f1f64 100%);
+}
 </style>
